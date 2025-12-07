e6d077fa655f4771870fd7ed153fae92933c2bd1
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2025-12-07 17:41:26 +0500

    feat/setup multi lingual support in documents

--- server/db/document.ts
+++ server/db/document.ts
@@ -3,14 +3,22 @@ import type { SchemaDefExt } from "../feathers/schema";
 
 const schema: SchemaDefExt = {
   name: {
-    en: { type: String, index: true,required:true, $editor: { label: "Name (English)" } },
+    en: { type: String, index: true, required: true, $editor: { label: "Name (English)" } },
     hy: { type: String, index: true, $editor: { label: "Name (Armenian)" } },
     ru: { type: String, index: true, $editor: { label: "Name (Russian)" } },
   },
   content: [
     {
-      heading: { type: String },
-      desc: { type: String, $editor: { props: { multiLine: true } } },
+      heading: {
+        en: { type: String, $editor: { label: "Heading (English)" } },
+        hy: { type: String, $editor: { label: "Heading (Armenian)" } },
+        ru: { type: String, $editor: { label: "Heading (Russian)" } },
+      },
+      desc: {
+        en: { type: String, $editor: { props: { multiLine: true }, label: "Description (English)" } },
+        hy: { type: String, $editor: { props: { multiLine: true }, label: "Description (Armenian)" } },
+        ru: { type: String, $editor: { props: { multiLine: true }, label: "Description (Russian)" } },
+      },
       images: [{ type: "id", ref: "Attachment", fileType: "image" }],
     },
   ],
